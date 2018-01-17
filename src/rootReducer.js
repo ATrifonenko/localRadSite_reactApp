2377e08cfbd5a41588c5e06339fb5c9783327343
Andrey Trifonenko <sintez91@gmail.com>
2018-01-17 18:26:09 +0500

    reset redux state after user logout

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -3,8 +3,18 @@ import user from "./reducers/user";
 import sidebar from "./reducers/sidebar";
 import dashboard from "./reducers/dashboard";
 
-export default combineReducers({
+const appReducer = combineReducers({
   user,
   sidebar,
   dashboard
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === 'USER_LOGGED_OUT') {
+    state = undefined
+  }
+
+  return appReducer(state, action)
+}
+
+export default rootReducer;
