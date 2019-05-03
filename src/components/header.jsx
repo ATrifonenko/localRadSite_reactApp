bd43f94838376e200ae11f30df147c6130b64224
Andrey Trifonenko <sintez91@gmail.com>
2019-05-03 16:13:45 +0500

    semantic menu

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Menu, Dropdown } from "semantic-ui-react";
 import "../css/header.css";
 import logo from "../media/gerb-mvd.png";
 
+// ссылки на сервисы ИСОД
+const links = [{ title: "Почта", link: "#" }, { title: "СЭД", link: "#" }];
+
 function Header() {
+  const dropMenu = links.map(link => (
+    <Dropdown.Item as="a" href={link.link} target="_blank">
+      {link.title}
+    </Dropdown.Item>
+  ));
+
   return (
     <header className="header">
       <div className="title-block">
         <Link to="/" className="link">
-          <img src={logo} alt="МВД г.Радужный" className="logo" />
+          <img src={logo} alt="ОМВД г.Радужный" className="logo" />
         </Link>
         <h1 className="title">Информационный сайт ОМВД по г.Радужному</h1>
       </div>
+
       <nav className="main-nav">
-        <Link className="menu-item" to="/">
-          <span>Новости</span>
-        </Link>
-        <Link className="menu-item" to="/phonebook">
-          <span>Справочник</span>
-        </Link>
-        <Link className="menu-item" to="/testing">
-          <span>Сервисы ИСОД</span>
-        </Link>
+        <Menu color="blue" inverted fluid widths={3} size="huge">
+          <Menu.Item name="editorials" as={Link} to="/">
+            Новости
+          </Menu.Item>
+          <Menu.Item name="reviews" as={Link} to="/phonebook">
+            Справочник
+          </Menu.Item>
+          <Dropdown
+            simple
+            item
+            text="Сервисы ИСОД"
+            as="a"
+            href="#" // main link
+            target="_blank"
+          >
+            <Dropdown.Menu color="blue" inverted>
+              {dropMenu}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu>
       </nav>
     </header>
   );
